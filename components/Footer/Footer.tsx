dba52e41ba8f9ce6488f92d659cca6d5465b1b25
Petar <pecata83@gmail.com>
2020-12-02 00:12:25 +0200

    add footer social icons

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import styles from "./Footer.module.css";
+import { FiFacebook } from "@react-icons/all-files/fi/FiFacebook";
+import { FiInstagram } from "@react-icons/all-files/fi/FiInstagram";
+import { FiLinkedin } from "@react-icons/all-files/fi/FiLinkedin";
 
 type Props = {
   title?: string;
@@ -8,6 +11,17 @@ type Props = {
 const Footer: React.FC<Props> = ({ title }) => (
   <footer className={styles.footer}>
     <div className={styles.footer_container}>
+      <ul className={styles.footer_icons}>
+        <li>
+          <FiLinkedin />
+        </li>
+        <li>
+          <FiFacebook />
+        </li>
+        <li>
+          <FiInstagram />
+        </li>
+      </ul>
       <p>Copyright 2020</p>
     </div>
   </footer>
